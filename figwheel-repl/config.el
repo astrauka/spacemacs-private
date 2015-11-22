ac1246b2556440a4cf889a9fd93196d54f4d1419
Karolis Astrauka <astrauka@gmail.com>
2015-11-22 15:35:33 +0200

    Update figwheel config

--- figwheel-repl/config.el
+++ figwheel-repl/config.el
@@ -1,7 +1,20 @@
-(add-hook 'cider-connected-hook '(lambda ()
-                                   (cider-interactive-eval
-                                    "(use 'figwheel-sidecar.repl-api)\n(cljs-repl)\n")))
+;; (add-hook 'cider-connected-hook '(lambda ()
+;;                                    (cider-interactive-eval
+;;                                     "(use 'figwheel-sidecar.repl-api)\n(cljs-repl)\n")))
 
-(defun figwheel-connect ()
+;; (defun figwheel-connect ()
+;;   (interactive)
+;;   (cider-connect "localhost" "7888"))
+
+(defun cider-figwheel-repl ()
   (interactive)
-  (cider-connect "localhost" "7888"))
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(require 'figwheel-sidecar.repl-api)
+             (figwheel-sidecar.repl-api/start-figwheel!) ; idempotent
+             (figwheel-sidecar.repl-api/cljs-repl)")
+    (cider-repl-return)))
+
+(evil-leader/set-key "cRr" 'cider-jack-in)
+(evil-leader/set-key "cRf" 'cider-figwheel-repl)
